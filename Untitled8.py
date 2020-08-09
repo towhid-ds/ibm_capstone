dc3f954e4f428b65aef50047917e43fe1412ab76
towhid-ds <69413400+towhid-ds@users.noreply.github.com>
2020-08-09 21:03:25 +0600

    Add files via upload

---
+++ Untitled8.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1> THIS IS IBM CAPSTONE PROJECT!
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
